f05ba462f3f717b8c9bbea8327a41bba888967d8
benongg <117260589+benongg@users.noreply.github.com>
2024-05-17 13:44:45 +0800

    Update Torchlight initial state to be off

--- Group 7/Assets/_Added Scripts/TorchLightToggle.cs
+++ Group 7/Assets/_Added Scripts/TorchLightToggle.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class TorchLightToggle : MonoBehaviour
 {
-   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure the torchlight is off at the start
+        gameObject.SetActive(false);
+    }
+
     public void Toggle()
     {
         gameObject.SetActive(!gameObject.activeSelf);
