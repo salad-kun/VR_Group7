32f533bc89869986b112ad8febd730ef0f252d88
salad-kun2 <169064801+salad-kun2@users.noreply.github.com>
2024-05-20 14:24:40 +0800

    To open cabinet door

---
+++ Group 7/Assets/_Added Scripts/RotateCabinetDoor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCabinetDoor : MonoBehaviour
+{
+    private bool isTouched = false;
+	
+	[SerializeField] private float angle = 90f;
+
+    public void toggleDoor()
+    {
+        isTouched = !isTouched;
+
+        if (isTouched)
+        {
+            transform.Rotate(Vector3.up, angle); // Rotate 90 degrees when switched on
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, -angle); // Rotate 90 degrees when switched on
+        }
+    }
+}
